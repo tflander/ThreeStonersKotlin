f5f30c10545748ded0f7064e61ba508c82d8087f
Todd Flanders <toddflanders@Todds-MacBook-Pro.local>
2018-04-23 18:19:49 -0400

    run as spring boot application

--- src/main/kotlin/todd/stoners/ThreeStonersApp.kt
+++ src/main/kotlin/todd/stoners/ThreeStonersApp.kt
@@ -2,31 +2,45 @@ package todd.stoners
 
 import kotlinx.coroutines.experimental.async
 import kotlinx.coroutines.experimental.runBlocking
+import org.springframework.boot.CommandLineRunner
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.Bean
+
+@SpringBootApplication
+class ThreeStonersApp {
+
+    @Bean
+    fun init() = CommandLineRunner {
+        println("*** Hello ***")
+        val messageQueue = StonerMessageQueue()
+
+        val hippyCircle = HippyCircle(messageQueue,
+                Pair("Todd", Material.WEED),
+                Pair("Harpreet", Material.MATCHES),
+                Pair("Jibin", Material.PAPERS)
+        )
+
+        val deferred = (hippyCircle.stoners).map { stoner ->
+            stoner.hippyCircle = hippyCircle
+            async {
+                stoner.start()
+            }
+        }
+
+        val firstStoner = hippyCircle.stoners.first()
+        messageQueue.sendMessage(Message(firstStoner.name, "", "Your turn to roll"))
+
+        runBlocking {
+            deferred.forEach() { deferredStoner ->
+                deferredStoner.await()
+            }
+        }
+        println("Done");
+    }
+}
 
 fun main(args: Array<String>) {
-
-    val messageQueue = StonerMessageQueue()
-
-    val hippyCircle = HippyCircle(messageQueue,
-            Pair("Todd", Material.WEED),
-            Pair("Harpreet", Material.MATCHES),
-            Pair("Jibin", Material.PAPERS)
-    )
-
-    val deferred = (hippyCircle.stoners).map { stoner ->
-        stoner.hippyCircle = hippyCircle
-        async {
-            stoner.start()
-        }
-    }
-
-    val firstStoner = hippyCircle.stoners.first()
-    messageQueue.sendMessage(Message(firstStoner.name, "", "Your turn to roll"))
-
-    runBlocking {
-        deferred.forEach() { deferredStoner ->
-            deferredStoner.await()
-        }
-    }
-    println("Done");
+    SpringApplication.run(ThreeStonersApp::class.java, *args)
 }
+
