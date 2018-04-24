c9689c4c2f481afc02bcf2b5267e26f29bf18772
Todd Flanders <toddflanders@Todds-MacBook-Pro.local>
2018-04-24 07:58:10 -0400

    proper message queue test

--- src/test/kotlin/todd/stoners/StonerMessageQueueTest.kt
+++ src/test/kotlin/todd/stoners/StonerMessageQueueTest.kt
@@ -1,26 +1,20 @@
 package todd.stoners
 
-import org.junit.Ignore
 import org.junit.Test
-import org.junit.runner.RunWith
-import org.mockito.Mock
-import org.mockito.Mockito
-import org.mockito.junit.MockitoJUnitRunner
+import assertk.assert
+import assertk.assertions.*
 
-@RunWith(MockitoJUnitRunner::class)
 class StonerMessageQueueTest {
-
-    @Mock
-    lateinit var processorFactory: StonerMessageProcessorFactory
 
     val messageQueue = StonerMessageQueue()
 
-    val todd = Stoner("Todd", Material.WEED, "Harpreet", messageQueue);
-
-    @Test @Ignore // TODO: fails because stoner::start() loop is not run
-    fun `stoner uses the processor factory to process messages from the message queue`() {
-        todd.processorFactory = processorFactory
-        messageQueue.sendMessage(Message("Todd", "System", "Roll one"))
-        Mockito.verify(processorFactory).processorFor("Roll one")
+    @Test
+    fun `send message pushes to stack`() {
+        val messages = messageQueue.messages
+        assert(messages).isEmpty()
+        val message = Message("Todd", "System", "Roll one")
+        messageQueue.sendMessage(message)
+        assert(messages).containsExactly(message)
     }
+
 }
